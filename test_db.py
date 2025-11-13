899681baa0a0b82d369d96b10931bd95ae4f23d7
Akshay Pasumarthy <74232389+akshaypasumarthy@users.noreply.github.com>
2025-11-13 17:54:46 +0100

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -379,7 +379,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         print("✓ Database tables created")
-    pory = int(os.environ.get('PORT',5000))
+    port = int(os.environ.get('PORT',5000))
     app.run(host = '0.0.0.0',port = port,debug=False)
     
 
